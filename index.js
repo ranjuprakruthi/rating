a59506a36b4814af24f7ba76c245abaf759c3775
ranjuprakruthi <ranju.growth.hacker@gmail.com>
2024-11-29 12:45:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ document.addEventListener("DOMContentLoaded", function () {
         event.preventDefault(); // Prevent default form submission
 
         // Collect form data
-        const formData = new FormData(form);
-
+const email = document.getElementById("email").value;
+        const name = document.getElementById("name").value;
+        
         // Send data to Google Apps Script
         fetch(scriptURL, {
             method: "POST",
